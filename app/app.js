f299409c2678b92b96e0a2d7b8922d4935d3c34c
C盘先生 <892399698@qq.com>
2015-12-07 09:48:32 +0800

    a

--- app/app.js
+++ app/app.js
@@ -2,17 +2,105 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import common from './common';
+
+//#test
+// var testData = {
+//     code: 1000
+// };
+
+
+var baseURL = location.protocol + "//" + location.host;
+var urls = {
+    base: baseURL,
+    apiBase: baseURL + "/blog_interface",
+    login: baseURL + "/users/sign_in"
+};
 
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
+    modulePrefix: config.modulePrefix,
+    podModulePrefix: config.podModulePrefix,
+    Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
 
-export default App;
+
+var injectAppData = function(data) {
+  console.log("injectAppData=>");
+  console.log(data);
+    // var user = data.user;
+    // var role_can = data.role_can || {};
+    // var role_no = {};
+    // var company = data.company || {};
+    // var can_columns = company.can_columns || {};
+    // for (var k in can_columns) {
+    //     var _val = can_columns[k];
+    //     if (_val == "true") _val = true;
+    //     else if (_val == "false") _val = false;
+
+    //     can_columns[k] = _val;
+    // }
+
+    // role_can.group_admin_user_or_company = role_can.group_admin_user || role_can.group_admin_company;
+
+    // for (var key in role_can) {
+    //     role_no[key] = !role_can[key];
+    // }
+
+    // common.injectData("role_can", Ember.Object.create(role_can));
+    // common.injectData("role_no", Ember.Object.create(role_no));
+    // common.injectData("callConfig", Ember.Object.create(data.ytxapp));
+    // common.injectData("can_columns", can_columns);
+    // common.injectData("company", company);
+    // common.injectData("custom_plugin", data.custom_plugin);
+    // common.injectData("currentAgents", data.agents);
+
+    // var UDD = _.extend(data, {
+    //     isAdmin: user.role == "admin",
+    //     isAgent: user.role == "agent",
+    //     role_can: role_can, //权限：能够访问的
+    //     role_no: role_no //权限：不能访问的
+    // });
+    // common.injectData("UDD", UDD);
+    // window.UDD = UDD;
+};
+
+// export default App;
+export default App.extend({
+    create: function(options) {
+        Ember.RSVP.all([
+            Ember.$.get(urls.apiBase + '/init_data')
+        ]).then(
+            function(initData) {
+                console.log(initData.code);
+                if (initData.code === 2003) {
+                    location.replace(urls.login);
+                }
+                if (initData.code !== 1000) {
+                    return;
+                }
+                delete initData.code;
+                console.log("do");
+                options.urls = urls;
+                options.apiPath = urls.apiBase;
+
+
+                Ember.merge(options, initData);
+
+                window.currentApp = App.create(options);
+                window.currentContainer = window.currentApp.__container__;
+                window.currentRouter = window.currentContainer.lookup("router:main");
+
+                injectAppData(options);
+
+            },
+            function(res) {
+                console.log(res);
+            });
+    }
+});
---
+++ app/common.js
@@ -0,0 +1,17 @@
+/*公用js*/
+
+var common = {
+    injectData: function(name, data, regType) {
+        var regName = (regType || "default") + ":" + name;
+        if (!name) return;
+
+        currentApp.register(regName, data || {}, {
+            instantiate: false
+        });
+        currentApp.inject("controller", name, regName);
+        currentApp.inject("route", name, regName);
+        currentApp.inject("component", name, regName);
+        currentApp.inject("model", name, regName);
+    },
+};
+export default common;
