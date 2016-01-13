02d3c65cfba584d2b26dd2bef41ad76761ea266d
C盘先生 <892399698@qq.com>
2016-01-13 14:57:11 +0800

    a

--- app/components/core/article/article-save.js
+++ app/components/core/article/article-save.js
@@ -3,16 +3,17 @@ var g = Ember.get;
 var s = Ember.set;
 export default Ember.Component.extend({
     actions: {
-        submit: (data) => {
+        submit(data){
             var self = this;
             if (!data) {
                 return false;
             }
+            var column_id=g(data, "column_id");
             if (!g(data, "title")) {
                 common.tips.error("文章标题不能为空！");
                 return false;
             }
-            if (!g(data, "column_id")) {
+            if (!column_id) {
                 common.tips.error("栏目不能为空！");
                 return false;
             }
@@ -25,7 +26,7 @@ export default Ember.Component.extend({
             }
             saveData.save().then((res) => {
                 common.tips.success("保存成功！");
-                slef.get("currentController").transitionToRoute("core.article");
+                self.get("currentController").transitionToRoute("core.article",column_id);
             }, (res) => {
                 var err = res.msg || "保存失败！";
                 common.tips.error(err);
--- app/models/article.js
+++ app/models/article.js
@@ -3,9 +3,12 @@ export default DS.Model.extend({
 	title:DS.attr(),
 	update_time:DS.attr(),
 	create_time:DS.attr(),
+  column_id:DS.attr(),
 	// column_id:DS.belongsTo("column"),
 	click:DS.attr(),
-	HTML:DS.attr(),
+	flag:DS.attr(),
 	status:DS.attr(),
-	editor:DS.attr(),
+  keyword:DS.attr(),
+  description:DS.attr(),
+  body:DS.attr()
 })
