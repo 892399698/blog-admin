5c8daf5e7809a33abb6fb3b0743f4fb38ce1df83
C盘先生 <892399698@qq.com>
2016-01-23 18:16:29 +0800

    a

--- app/controllers/core/column/show.js
+++ app/controllers/core/column/show.js
@@ -1,11 +1,12 @@
-var g=Ember.get;
+import common from 'blog-admin/common';
+var g = Ember.get;
 export default Ember.Controller.extend({
     formatArticles: function() {
         var formatArticles = this.get("articles");
         var initColumns = this.get("initColumns") || [];
         // console.log(initColumns)
         formatArticles && formatArticles.length && formatArticles.forEach((_item) => {
-            var id = g(_item,"column_id");
+            var id = g(_item, "column_id");
             var column = initColumns.findBy("id", id) || {};
             var column_name = g(column, "name");
             Ember.set(_item, "column_name", column_name || "");
@@ -13,7 +14,39 @@ export default Ember.Controller.extend({
         console.log(formatArticles)
         return formatArticles;
     }.property("articles"),
-    actions:{
-        
+    actions: {
+        delete(id) {
+                if (!id) {
+                    common.tips.error("文章id为空!");
+                    return false;
+                }
+                this.setProperties({
+                    "ifDialog": true,
+                    dialogContent: "确定删除文章么?",
+                    dialogCallback: "deleteCallback",
+                    delId: id
+                });
+            },
+            deleteCallback: function() {
+                var self = this;
+                var ajaxing = self.get("ajaxing");
+                if (ajaxing) {
+                    return false;
+                }
+                self.set("ajaxing", true);
+                Ember.$.ajax({
+                    method: "DELETE",
+                    url: UDD.urls.apiBase + "/articles/" + this.get("delId"),
+                }).then(function(res) {
+                    if (res.code === 1000) {
+                        self.send("refreshModel");
+                    } else {
+                        common.tips.error(res.msg);
+                    }
+                }).always(function() {
+                    self.set("ajaxing", false);
+                    self.set("ifDialog", false);
+                })
+            }
     }
 })
--- app/routes/core/column/show.js
+++ app/routes/core/column/show.js
@@ -1,48 +1,51 @@
 import common from 'blog-admin/common';
 export default Ember.Route.extend({
-	model:function(params){
-		var id=params.id;
-		if(!id){
-			common.tips.error("栏目id不能为空!");
-			this.transitionTo("core.column");
-			return false;	
-		}
-		var articlesDefer=Ember.RSVP.defer();
-		// this.store.find("article",id).then(function(res){
-		// 	console.log(res)
-		// 	articlesDefer.resolve(res);
-		// },function(err){
-		// 	articlesDefer.reject(err);
-		// })
-		var p={
-			column_id:id
-		}
-		Ember.$.get(UDD.urls.apiBase+"/articles",p).then(function(res){
-			if(res.code===1000){
-				articlesDefer.resolve(res.articles);
-			}else{
-				articlesDefer.reject(res.msg);
-				// common.tips.error(res.msg)
-			}
-		},function(err){
-			articlesDefer.reject("获取文章列表的错误!");
-		})
-		// this.store.find("article",id).then(function(res){
-		// 	console.log(res)
-		// 	articlesDefer.resolve(res);
-		// },function(err){
-		// 	articlesDefer.reject(err);
-		// })
-		return Ember.RSVP.hash({
-			articles:articlesDefer.promise
-		})
-	},
-	setupController:function(c,m){
-		c.setProperties(m);
-	},
-	actions:{
-		error:function(err){
-			common.tips.error(err);
-		}
-	}
+    model: function(params) {
+        var id = params.id;
+        if (!id) {
+            common.tips.error("栏目id不能为空!");
+            this.transitionTo("core.column");
+            return false;
+        }
+        var articlesDefer = Ember.RSVP.defer();
+        // this.store.find("article",id).then(function(res){
+        // 	console.log(res)
+        // 	articlesDefer.resolve(res);
+        // },function(err){
+        // 	articlesDefer.reject(err);
+        // })
+        var p = {
+            column_id: id
+        }
+        Ember.$.get(UDD.urls.apiBase + "/articles", p).then(function(res) {
+                if (res.code === 1000) {
+                    articlesDefer.resolve(res.articles);
+                } else {
+                    articlesDefer.reject(res.msg);
+                    // common.tips.error(res.msg)
+                }
+            }, function(err) {
+                articlesDefer.reject("获取文章列表的错误!");
+            })
+            // this.store.find("article",id).then(function(res){
+            // 	console.log(res)
+            // 	articlesDefer.resolve(res);
+            // },function(err){
+            // 	articlesDefer.reject(err);
+            // })
+        return Ember.RSVP.hash({
+            articles: articlesDefer.promise
+        })
+    },
+    setupController: function(c, m) {
+        c.setProperties(m);
+    },
+    actions: {
+        error: function(err) {
+            common.tips.error(err);
+        },
+        refreshModel: function() {
+            this.refresh();
+        }
+    }
 })
